700a1ae3266770a7e9abca8fef1ac427624fed2a
jihyun-jeon <jhplus13@naver.com>
2024-12-04 00:28:58 +0900

    feat: 프로미스에서 또 프로미스 반환하는 로직 개선 : 반환된 값이 프로미스인 경우 then호출, 아닌 경우 바로 resolve 하도록 구분

--- promise-Implementing/promise.ts
+++ promise-Implementing/promise.ts
@@ -51,31 +51,41 @@ class MyPromise_<T> {
   }
 
   then<U>(
-    thenCallback?: (x: T) => MyPromise_<U> | Promise<U>,
-    catchCallback?: (ex: any) => MyPromise_<U> | Promise<U>
+    thenCallback?: (x: T) => MyPromise_<U> | Promise<U> | U,
+    catchCallback?: (ex: any) => MyPromise_<U> | Promise<U> | U
   ): MyPromise_<U> {
     return new MyPromise_<U>((resolve, reject) => {
-      this.thenCallbacks.push(async (value) => {
+      this.thenCallbacks.push((value) => {
         if (!thenCallback) {
           resolve(value as unknown as U);
           return;
         }
+
         try {
-          const v = await thenCallback(value);
-          resolve(v);
+          const result = thenCallback(value);
+          if (result instanceof MyPromise_ || result instanceof Promise) {
+            (result as MyPromise_<any>).then(resolve, reject);
+          } else {
+            resolve(result);
+          }
         } catch (error) {
           reject(error);
         }
       });
 
-      this.catchCallbacks.push(async (value) => {
+      this.catchCallbacks.push((value) => {
         if (!catchCallback) {
           reject(value);
           return;
         }
+
         try {
-          const v = await catchCallback(value);
-          resolve(v);
+          const result = catchCallback(value);
+          if (result instanceof MyPromise_ || result instanceof Promise) {
+            (result as MyPromise_<any>).then(resolve, reject);
+          } else {
+            resolve(result as U);
+          }
         } catch (error) {
           reject(error);
         }
@@ -103,7 +113,7 @@ class MyPromise_<T> {
   }
 }
 
-// 실행 - promise 또 반환 ----------------------------------
+// 실행 - promise에서 promise 또 반환 ----------------------------------
 const promise = new MyPromise_((resolve, reject) => {
   setTimeout(() => {
     resolve("성공1");
@@ -111,23 +121,34 @@ const promise = new MyPromise_((resolve, reject) => {
 });
 
 promise
-  .then(async (result) => {
-    console.log("then-1", result); // ✔️ 성공1
+  .then((result) => {
+    console.log("then-1", result); //  then-1 성공1
     return new MyPromise_((resolve, reject) => {
-      setTimeout(async () => {
-        resolve("Succuess1");
-      }, 1000);
+      resolve("성공2");
     });
   })
-  .then(async (result) => {
-    console.log("then-2", result); // ✔️  "Succuess1"
-    throw new Error();
-  })
-  .then(async (result) => {
-    console.log("catch-1", result);
-    throw "성공2";
-  })
-  .catch(async (result) => {
-    console.log("then-3", result); // ✔️ Error
-    return "성공4";
+  .then((result) => {
+    console.log("then-2", result); // then-2 성공2
+    return "성공3";
   });
+
+/**
+ * 노트
+ *
+ * <then에서 또 프로미스 반환되는 로직>
+ * [1] thenCallback(value);실행 -> 서브 프로미스 반환 -> 서브 프로미스 resolve 실행
+ * -> update(fillfied , 성공2) -> 큐에 서브 프로미스 콜백 등록 (서브프로미스 resolve대기상태)
+ *
+ * [2] 동기 코드 남은거 실행  const result = 서브 프로미스{queue에서 resolve 대기상태}
+ * result가 프로미스 인스턴스 객체여서
+ * if (result instanceof MyPromise_ || result instanceof Promise) {} 에 걸림
+ *
+ * [3] result.then("resolve") 실행. 서브 프로미스{큐 resolve 대기}.then(resolve) 실행되는 것임.
+ * 이때의 resolve는 then프로미스를 this로 하는 resolve임!!!
+ *
+ * [4] 서브 프로미스의 this에 this.thenCallbacks에 Push 하고 끝
+ * [5] 동기 다 끝났으니, 큐에 넣어놓은 서브 프로미스 콜백 실행됨. this.value = 성공2
+ * [6] this.runCallbacks 실행 -> 4에서 push헤놔서 저장된 thenCbs 실행
+ * [7] resolve(성공2)가 실행되는데, 이때 resolve는 then프로미스꺼임
+ * 때문에 then 프로미스의 값이 리졸브되서 -> 다음 then으로 채이닝
+ */

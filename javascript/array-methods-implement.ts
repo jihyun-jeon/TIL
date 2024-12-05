0fc61e4b8618eca1a0d07611a1993c111bf98076
jihyun-jeon <jhplus13@naver.com>
2024-12-05 19:12:12 +0900

    feat: flatMap으로 map 구현하기

--- javascript/array-methods-implement.ts
+++ javascript/array-methods-implement.ts
@@ -42,6 +42,7 @@ function filter<T>(
 // console.log(filter(["a", "bb", "cccc", "dddd"], (s) => s.length <= 2)); // ["a","bb"]
 
 // 3. <flatMap> : flatMap(callbackFn(currentValue[, index[, array]]), thisArg)
+// flatMap의 callback의 리턴값은 배열인데, 그 배열을 한단계 평탄화한 값이 flatMap의 리턴값이 된다.
 function flatMap<T, U>(
   arr: T[],
   callback: (el: T, index: number, originalArr: T[]) => U[],
@@ -57,6 +58,32 @@ function flatMap<T, U>(
   return result;
 }
 
+// 4) flatMap으로 map 구현하기
+// flatMap의 callback의 리턴값은 배열인데, 그 배열을 한단계 평탄화 하지 않은 배열 그대로의 값이 flatMap2의 리턴값이 된다.
+function flatMap2<T, U>(
+  arr: T[],
+  callback: (el: T, index: number, originalArr: T[]) => U[],
+  thisArg?: any
+): U[] {
+  const result: U[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const value = callback.call(thisArg, arr[i], i, arr);
+    result.push(value);
+  }
+
+  return result;
+}
+
 // console.log(flatMap([1, 2, 3], (n) => [n, n * 2])); // [1, 2, 2, 4, 3, 6]
 // console.log(flatMap(["hello", "world"], (word) => word.split(""))); // ['h', 'e', 'l', 'l', 'o', 'w', 'o',..]
 // console.log(flatMap([[1], [2], [3], [4]], (el) => el.map((n) => n * 2))); // [ 2, 4, 6, 8 ]
+
+/*
+[노트]
+
+[P1] flatMap이 map 보다 자유도가 높다.
+왜? : map은 동일한 길이의 배열만 나와야 하는데, flatMap은 길이 다르게 나와도 된다.
+때문에 flatMap으로 map을 구현할 수 있지만, map으로 flatMap을 구현할 수 없다.
+
+*/
